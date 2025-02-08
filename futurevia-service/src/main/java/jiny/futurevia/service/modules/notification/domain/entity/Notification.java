e7c674e49a4111f1c5da9d9730dfd0072535a04d
jiny <jyoung7988@naver.com>
2025-02-09 01:55:49 +0900

    알림 조회, 삭제 추가

--- futurevia-service/src/main/java/jiny/futurevia/service/modules/notification/domain/entity/Notification.java
+++ futurevia-service/src/main/java/jiny/futurevia/service/modules/notification/domain/entity/Notification.java
@@ -43,4 +43,8 @@ public class Notification {
         notification.notificationType = notificationType;
         return notification;
     }
+
+    public void read() {
+        this.checked = true;
+    }
 }
