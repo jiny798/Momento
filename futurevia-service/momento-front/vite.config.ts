7a5550b0e2836fff4e95057d1360334bee702e30
jiny <jyoung7988@naver.com>
2025-03-28 00:04:28 +0900

    setup front

---
+++ futurevia-service/momento-front/vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
